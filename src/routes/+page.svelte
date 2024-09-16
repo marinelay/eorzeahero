b81d23fcd2ca106f4365c2c93131f0a9dbc5ead9
Oh Wonseok <marinelay7@gmail.com>
2024-09-16 23:12:35 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+    import { Carousel } from 'flowbite-svelte';
+    import images from './imageData/carousels.json';
+    import { Heading, P, Button } from 'flowbite-svelte';
+    import { ArrowRightOutline } from 'flowbite-svelte-icons';
+
+    import { Blockquote } from 'flowbite-svelte';
+    import { QuoteSolid } from 'flowbite-svelte-icons';
+
+    import { page } from '$app/stores';
+    // import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+    $: activeUrl = $page.url.pathname;
+
+    import Navbar from './Navbar.svelte';
+</script>
+
+<Navbar/>
+
+<div class="text-center">
+<Heading tag="h1" class="mt-8 mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">
+    에오르제아 용사
+</Heading>
+<P class="text-center mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+    용사들이 모인 공간입니다. 여러분들의 이야기를 들려주세요.
+</P>
+<!-- <Button href="/">
+    Learn more
+    <ArrowRightOutline class="w-6 h-6 ms-2" />
+</Button> -->
+</div>
+
+<div class="max-w-4xl container">
+<Carousel {images} let:Indicators let:Controls duration={6000}>
+    <Controls />
+    <Indicators />
+</Carousel>
+</div>
+
+
+<figure class="mt-8 mx-auto max-w-screen-md text-center">
+<QuoteSolid class="mx-auto mb-3 w-12 h-12 text-gray-400 dark:text-gray-600" />
+<Blockquote alignment="center" size="xl">
+    겁쟁이는 죽기 전에 여러 번 죽지만, 용사는 한 번밖에 죽지 않소. <br/>
+    - 윌리엄 셰익스피어
+</Blockquote>
+
+</figure>
+
+
+<style>
+.container {
+    margin: 0 auto;
+}
+</style>
---
+++ src/routes/member/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+    import Navbar from '../Navbar.svelte';
+
+    import { Card, Avatar, Button } from 'flowbite-svelte';
+</script>
+
+<Navbar/>
+
+<div class="card-container">
+    <Card class='card' padding="md">
+    <div class="flex flex-col items-center pb-4">
+        <img class="p-8 rounded-t-lg" src="/images/profile/ponytail.webp" alt="product 1" />
+        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">포니테일검사여자</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400">용사가 되고 싶은 자는 나에게...</span>
+        <div class="flex mt-4 space-x-3 rtl:space-x-reverse lg:mt-6">
+        <!-- <Button>Add friend</Button>
+        <Button color="light" class="dark:text-white">Message</Button> -->
+        </div>
+    </div>
+    </Card>
+
+    <Card class='card' padding="md">
+    <div class="flex flex-col items-center pb-4">
+        <img class="p-8 rounded-t-lg" src="/images/profile/songdowon.webp" alt="product 1" />
+        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">박사송도원</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400">용사가 되고 싶은 자는 나에게...</span>
+        <div class="flex mt-4 space-x-3 rtl:space-x-reverse lg:mt-6">
+        <!-- <Button>Add friend</Button>
+        <Button color="light" class="dark:text-white">Message</Button> -->
+        </div>
+    </div>
+    </Card>
+</div>
+
+<style>
+    .card-container {
+        display: flex;
+        justify-content: center;
+        gap: 16px;
+    }
+
+    .card {
+        flex: 1 0 auto; /* 카드가 컨테이너의 가용 공간을 채우도록 설정 */
+        min-width: 200px; /* 카드의 최소 너비 설정 */
+        max-width: 300px; /* 카드의 최대 너비 설정 */
+        box-sizing: border-box; /* padding과 border가 포함된 너비 계산 */
+    }
+</style>
